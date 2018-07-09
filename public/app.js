e44a7dd6ab5340f1d459ddf7526bad98e0e1fcfc
EC2 Default User <ec2-user@ip-172-31-16-101.us-east-2.compute.internal>
2018-07-09 15:59:53 +0000

    fetch and show todo list items

--- public/app.js
+++ public/app.js
@@ -1,1 +1,20 @@
-alert('it works')
+// tell cloud9 linter to not bother about jquery $
+/* global $ */
+
+$(function() {
+  // DOM is ready, request todos
+  $.getJSON('/api/todos') // relative URL since we are requesting our own server 
+  .then(addTodos)         // we don't care to much about error handling now
+  
+  
+});
+
+function addTodos(todos) {
+    var todoList = $('.list');
+    todos.forEach(function(todo) {
+        var listItem =  $('<li class="task"></li>')
+        .html(todo.name)
+        .toggleClass('done', todo.completed)
+        .appendTo(todoList)
+    });
+}
