4cd0054427fa2277af7c49f1718fa2d79d8cbdcb
EC2 Default User <ec2-user@ip-172-31-16-101.us-east-2.compute.internal>
2018-07-09 14:04:09 +0000

    getting todos via id works

--- routes/todos.js
+++ routes/todos.js
@@ -9,16 +9,18 @@ ALL REQUESTS ARE PREFIXED WITH /api/todos/ DUE TO THE app.use CALL in index.js
 
 */
     
+// SHOW ALL ROUTE
 router.get('/', function(req, res) { 
     db.Todo.find()
     .then(function(todos) {
         res.json(todos)
     })
-    .catch(function() {
+    .catch(function(err) {
         res.send(err)
     })
 });
 
+// CREATE ROUTE
 router.post('/', function(req, res) {
     // log req body
     console.log('received todo', req.body)
@@ -26,12 +28,26 @@ router.post('/', function(req, res) {
     db.Todo.create(req.body)
     .then(function(newTodo) {
         res
-        .status(291)
+        .status(201)
         .send(newTodo)
     })
-    .catch(function() {
+    .catch(function(err) {
         res.send(err)
     })
 })
+
+// SHOW TODO ITEM ROUTE
+
+router.get('/:todoId', function(req, res) { // todoId is a varname for the passed value
+   db.Todo.findById(req.params.todoId)      // extract the db item matching the passed id
+    .then(function(foundTodo) {
+        res
+        .status(200)
+        .send(foundTodo)
+    })
+    .catch(function(err) {
+        res.send(err)
+    })
+}); 
 
 module.exports = router;
