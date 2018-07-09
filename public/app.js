14db8bc758683caa6576c3daf0b64d0619026c0f
EC2 Default User <ec2-user@ip-172-31-16-101.us-east-2.compute.internal>
2018-07-09 17:19:02 +0000

    update complete property

--- public/app.js
+++ public/app.js
@@ -18,16 +18,23 @@ $(function() {
       }
   })
   
+  // delete item on X button click
   // listeners to todo items must be attached to the list since it is there when the script runs
   // --> todos are being fetched async and then added as list elements
-  $('.list').on('click', 'span.deleteTodo', function() {
+  $('.list').on('click', 'span.deleteTodo', function(e) {
+      e.stopPropagation(); // stop bubbling up the click event so that no click on li is triggered
       // get reference on parent element of clicked X item (the list element)
        var $parentLi = $(this).parent();
        deleteTodo($parentLi)
-      
   });
+  
+  // update todo.completed on list item click
+  $('.list').on('click', 'li.task', function() {
+      var $li = $(this);
+      updateTodo($li);
+  });
+  
 });
-
 
 function showTodos(todos) {
     todos.forEach(function(todo) {
@@ -41,6 +48,7 @@ function showTodo(todo) {
         .html(todo.name)
         .toggleClass('done', todo.completed)
         .data('id', todo._id)
+        .data('completed', todo.completed)
         .append($('<span class="deleteTodo">X</span>'))
         .appendTo($todoList)
 }
@@ -64,4 +72,21 @@ function deleteTodo($todo) {
           // remove dom element
           $todo.remove();
       })
+}
+
+function updateTodo($li) {
+    var todoId = $li.data('id');
+    var url = API + '/' + todoId;
+    var completed = $li.data('completed');
+      
+    $.post(url, {completed: !completed})
+      .then(function() {
+        $li.data('completed', !completed);  
+        $li.toggleClass('done')
+        // having to update the view and the view model shows the limitations of jquery.
+        // frameworks like react would offer us binding the view property to the underlying data
+      })
+      .catch(console.error.bind(console))
+      
+  
 }
