bedbbea37975a30ff9d2882d365f2327016c9518
EC2 Default User <ec2-user@ip-172-31-16-101.us-east-2.compute.internal>
2018-07-09 16:29:07 +0000

    submit todo using form input

--- public/app.js
+++ public/app.js
@@ -1,20 +1,45 @@
 // tell cloud9 linter to not bother about jquery $
 /* global $ */
 
+
+var API = '/api/todos';
+var $todoInput = $('#todoInput');
+
 $(function() {
   // DOM is ready, request todos
-  $.getJSON('/api/todos') // relative URL since we are requesting our own server 
-  .then(addTodos)         // we don't care to much about error handling now
+   $.getJSON(API) // relative URL since we are requesting our own server 
+        .then(addTodos)         // we don't care to much about propper error handling now
+        .catch(console.error.bind(console))
   
-  
+  // on enter press in input, submit todo
+  $todoInput.keypress(function(event) {
+      if (event.which == 13) {
+          submitTodo($todoInput.val());
+      }
+  })
 });
 
+
+
 function addTodos(todos) {
-    var todoList = $('.list');
     todos.forEach(function(todo) {
-        var listItem =  $('<li class="task"></li>')
+        addTodo(todo);
+    })
+}
+
+function addTodo(todo) {
+    var $todoList = $('.list');
+    var newTodo = $('<li class="task"></li>')
         .html(todo.name)
-        .toggleClass('done', todo.completed)
-        .appendTo(todoList)
-    });
+        .toggleClass('done', todo.completed) 
+        .appendTo($todoList)
+}
+
+function submitTodo(name) {
+    $.post(API, {name: name})
+    .then(function(newTodo) {
+        addTodo(newTodo);
+        $todoInput.val('');
+    })
+    .catch(console.error.bind(console))
 }
