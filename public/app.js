55389bf4c5cfee4aaf6f3b4eab80970bdfae346d
EC2 Default User <ec2-user@ip-172-31-16-101.us-east-2.compute.internal>
2018-07-09 16:52:11 +0000

    delete todo items

--- public/app.js
+++ public/app.js
@@ -8,7 +8,7 @@ var $todoInput = $('#todoInput');
 $(function() {
   // DOM is ready, request todos
    $.getJSON(API) // relative URL since we are requesting our own server 
-        .then(addTodos)         // we don't care to much about propper error handling now
+        .then(showTodos)         // we don't care to much about propper error handling now
         .catch(console.error.bind(console))
   
   // on enter press in input, submit todo
@@ -17,28 +17,43 @@ $(function() {
           submitTodo($todoInput.val());
       }
   })
+  
+  // listeners to todo items must be attached to the list since it is there when the script runs
+  // --> todos are being fetched async and then added as list elements
+  $('.list').on('click', 'span.deleteTodo', function() {
+      // get reference on parent element of clicked X item (the list element)
+       var $parentLi = $(this).parent();
+      // get todo id, then do DELETE req for todo
+      $.ajax({
+          method: 'DELETE',
+          url: API + '/' + $parentLi.data('id')
+      })
+      // remove dom element
+      $parentLi.remove();
+  });
 });
 
 
-
-function addTodos(todos) {
+function showTodos(todos) {
     todos.forEach(function(todo) {
-        addTodo(todo);
+        showTodo(todo);
     })
 }
 
-function addTodo(todo) {
+function showTodo(todo) {
     var $todoList = $('.list');
     var newTodo = $('<li class="task"></li>')
         .html(todo.name)
-        .toggleClass('done', todo.completed) 
+        .toggleClass('done', todo.completed)
+        .data('id', todo._id)
+        .append($('<span class="deleteTodo">X</span>'))
         .appendTo($todoList)
 }
 
 function submitTodo(name) {
     $.post(API, {name: name})
     .then(function(newTodo) {
-        addTodo(newTodo);
+        showTodo(newTodo);
         $todoInput.val('');
     })
     .catch(console.error.bind(console))
