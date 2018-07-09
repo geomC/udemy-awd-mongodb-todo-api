913521c02b73ce1e67474df12eec7ec45d20713b
EC2 Default User <ec2-user@ip-172-31-16-101.us-east-2.compute.internal>
2018-07-09 14:53:24 +0000

    make app serve static content

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ var express = require("express"),
     
 var todoRoutes = require('./routes/todos');
 
-
-app.get('/', function(req, res) {
-    res.send('HELLO FROM THE ROOT ROUTE')
-})
-
 // make usage of body-parser to parse post req bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// serve files in views folder as static content
+app.use(express.static(__dirname + '/views'));
+
+// todo app client
+app.get('/', function(req, res) {
+    res.sendFile('index.html')
+})
+
+// todo app api
 app.use(
     '/api/todos', // prefix for all routes
     todoRoutes
